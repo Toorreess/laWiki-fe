8e3350ea66e0060b4f7ba18b79806090b8682fc4
Alacesp <1c.alvaroacedoespejo@uma.es>
2024-12-08 12:07:43 +0100

    users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'user', component: UserDetailsComponent }
+];
