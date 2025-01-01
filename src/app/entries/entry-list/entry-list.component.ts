072a384e0756279c6f3300f3b0f775421cb2f611
Jose Torres <94240050+Toorreess@users.noreply.github.com>
2025-01-01 17:40:36 +0100

    Revert "Feat/users"

--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-lista-entradas',
-  standalone: true,
-  imports: [RouterModule],
+  selector: 'app-entry-list',
+  imports: [],
   templateUrl: './entry-list.component.html',
-  styleUrls: ['./entry-list.component.css']
+  styleUrl: './entry-list.component.css'
 })
 export class EntryListComponent {
-  static entradas = [
-    { titulo: 'Entrada 1', contenido: 'Descripcion de la entrada 1', autor: 'Username' },
-    { titulo: 'Entrada 2', contenido: 'Descripcion de la entrada 2', autor: 'autor2' },
-    { titulo: 'Entrada 3', contenido: 'Descripcion de la entrada 3', autor: 'Username' }
-  ];
+
 }
