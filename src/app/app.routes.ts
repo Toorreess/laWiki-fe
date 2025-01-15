7fd02e53bb7d962b28fcfac5199a34f3cdac45d9
JRodrigoRC <0619985798@uma.es>
2025-01-15 19:15:41 +0100

    Entry details funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { WikiListComponent } from './wikis/wiki-list/wiki-list.component';
 import { WikiDetailsComponent } from './wikis/wiki-details/wiki-details.component';
 import { WikiCreateComponent } from './wikis/wiki-create/wiki-create.component';
 import { EntryFormComponent } from './entries/entry-form/entry-form.component'; 
+import { EntryDetailsComponent } from './entries/entry-details/entry-details.component'; 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/wikis', pathMatch: 'full' },
@@ -10,6 +11,8 @@ export const routes: Routes = [
   { path: 'wikis/create', component: WikiCreateComponent },
   { path: 'wikis/:id', component: WikiDetailsComponent },
   { path: 'wikis/:id/create-entry', component: EntryFormComponent }, 
+  { path: 'entries/:id', component: EntryDetailsComponent }, // Cambié 'entry' por 'entries'
 ];
+
 
 
