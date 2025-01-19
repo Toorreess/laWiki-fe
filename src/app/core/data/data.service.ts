e99191a44eeb9995d60bdd885418d2cfc98aacfa
Toorreess <jostorpos@uma.es>
2025-01-19 16:02:18 +0100

    ✨ feat: data service

---
+++ src/app/core/data/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  private api_url = 'http://localhost:8080/api';
+
+  public getEntitites(endpoint: string, params: any = {}) {
+    return this.http.get(`${this.api_url}/${endpoint}`, { params });
+  }
+
+  public getEntity(endpoint: string, id: string) {
+    return this.http.get(`${this.api_url}/${endpoint}/${id}`);
+  }
+
+  public createEntity(endpoint: string, data: any) {
+    return this.http.post(`${this.api_url}/${endpoint}`, data);
+  }
+
+  public updateEntity(endpoint: string, id: string, data: any) {
+    return this.http.put(`${this.api_url}/${endpoint}/${id}`, data);
+  }
+
+  public deleteEntity(endpoint: string, id: string) {
+    return this.http.delete(`${this.api_url}/${endpoint}/${id}`);
+  }
+}
